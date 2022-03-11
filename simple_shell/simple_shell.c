5ce685dd6f1cce9334577116667369498cb122f2
Jenna Pasternak <jennap2@umbc.edu>
2022-03-11 14:50:14 -0500

    new commit

--- simple_shell/simple_shell.c
+++ simple_shell/simple_shell.c
@@ -1,10 +1,41 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 #include "utils.h"
 
+char *command_loop(void);
+char **parse(char *input, int*);
+
 int main(int argc, char *argv[]){
-	char input;
-	while(input != "exit"){
-		printf("Command Prompt: ");
-		scanf("%c", &input);
-	}
+	command_loop();
 	return 0;
+}
+
+// Get input from the user
+char *command_loop(void){
+	char *input = NULL;
+	char **input_parsed = NULL;
+	size_t buff = 4;
+	int pos = 0;
+	do {
+		printf(">> ");
+		getline(&input, &buff, stdin);
+		if(strcmp(input, "exit") == 0){
+			free(input);
+			exit(0);
+		}
+		input_parsed = parse(input, &pos);
+		for(int i = 0; i < pos; i++){
+			free(input_parsed[i]);
+		}
+		free(input_parsed);
+	} while(input != "exit");
+	return input;
+}
+
+// Parse the user input string
+char **parse(char *input, int *pos){
 }
