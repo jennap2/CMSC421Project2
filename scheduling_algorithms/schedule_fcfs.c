9d2fc097dc2fb7618a5544926afee5e97e19d14a
Jenna Pasternak <jennap2@umbc.edu>
2022-03-14 02:53:28 -0400

    New commit part 3

--- scheduling_algorithms/schedule_fcfs.c
+++ scheduling_algorithms/schedule_fcfs.c
@@ -4,9 +4,10 @@
 #include "schedulers.h"
 
 /*GLOBAL VARIABLE*/
-struct node *temp;
+struct node *tempNode;
 
 void pickNextTask();
+void recursiveBackwards(struct node*);
 
 // add a task to the list
 void add(char *name, int priority, int burst){
@@ -14,12 +15,30 @@ void add(char *name, int priority, int burst){
         tempTask->name = name;
         tempTask->priority = priority;
         tempTask->burst = burst;
-        insert(&temp, tempTask);
+        insert(&tempNode, tempTask);
         printf("Inserted: %s \n", tempTask->name);
 }
 
 // invoke the fcfs scheduler
 void schedule(){
-	traverse(temp);
+	traverse(tempNode);
 	printf("The First-Come, First-Served scheduling algorithm schedules tasks in the order in which they request the CPU\n");
+	pickNextTask();
+}
+
+// Run and remove from the list in order of first to last request
+void pickNextTask(){
+	struct node *tempHead = tempNode;
+	recursiveBackwards(tempHead);
+}
+
+// Recursively goes through out linked list backwards 
+void recursiveBackwards(struct node* headPoint){
+        if(headPoint == NULL){
+                return;
+        }
+	recursiveBackwards(headPoint->next);
+        run(headPoint->task,headPoint->task->burst);
+        printf("Remove %s\n", headPoint->task->name);
+        delete(&tempNode,headPoint->task);
 }
--- scheduling_algorithms/schedule_rr.c
+++ scheduling_algorithms/schedule_rr.c
@@ -4,9 +4,10 @@
 #include "schedulers.h"
 
 /*GLOBAL VARIABLE*/
-struct node *temp;
+struct node *tempNode;
 
-void pickNextTask(Task *task, int slice);
+void pickNextTask();
+void reverseList();
 
 // add a task to the list
 void add(char *name, int priority, int burst){
@@ -14,12 +15,43 @@ void add(char *name, int priority, int burst){
         tempTask->name = name;
         tempTask->priority = priority;
         tempTask->burst = burst;
-        insert(&temp, tempTask);
+        insert(&tempNode, tempTask);
         printf("Inserted: %s \n", tempTask->name);
 }
 
 // invoke the rr scheduler
 void schedule(){
-	traverse(temp);
+	traverse(tempNode);
 	printf("The Round-Robin scheduling algorithm schedules tasks where each task in run for a time quantum (or for the remainder of its CPU burst)\n");
+	pickNextTask();
+}
+
+// Run round robin
+void pickNextTask(){
+	reverseList();
+	struct node *tempHead = tempNode;
+	int quantum = 10;
+	int sliceArray[]
+	while(tempHead != NULL){
+		// Run
+		run(tempHead->task,tempHead->task->burst);
+        	// Remove
+		printf("Remove %s\n", tempHead->task->name);
+        	delete(&tempNode,tempHead->task);
+	}
+}
+
+// Recursively goes through linked list backwards 
+void reverseList(){
+        struct node *currNode = tempNode;
+	struct node *prevNode = NULL, *nextNode = NULL;
+	while(currNode != NULL){
+		nextNode = currNode->next;
+		currNode->next = prevNode;
+		prevNode = currNode;
+		currNode = nextNode;
+	}
+	tempNode = prevNode;
+	printf("List reversed:\n");
+	traverse(tempNode);
 }
